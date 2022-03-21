d1c43c96f5d81678f1392bd168b4499fbaf01f2e
rustam20.04 <rust-shafik@yandex.ru>
2022-03-21 23:08:16 +0500

    new

--- src/test/java/ru/gb/MyFirstAppiumTest.java
+++ src/test/java/ru/gb/MyFirstAppiumTest.java
@@ -24,15 +24,25 @@ public class MyFirstAppiumTest {
     capabilities.setCapability("automationName", "UiAutomator2");
     capabilities.setCapability("app", "/Users/rustamsafikov/Downloads/Android-NativeDemoApp-0.2.1.apk");
     MobileDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    MobileElement loginMenuButton = (MobileElement) driver.findElementByAccessibilityId("Login");
-    loginMenuButton.click();
+
+    MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("Login");
+    el1.click();
     Thread.sleep(2000);
-//      Нажимаем Login на странице логин/пароль.
-    MobileElement loginButton = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
-    loginButton.click();
+
+    MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("input-email");
+    el2.click();
+    el2.sendKeys("Rustam");
+
+    MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("input-password");
+    el3.click();
+    el3.sendKeys("220437");
+
+    MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("button-LOGIN");
+    el4.click();
+
     Thread.sleep(2000);
-//      Проверяем текст ошибки.
     MobileElement errorText = (MobileElement) driver.findElementByXPath("//android.widget.ScrollView[@content-desc=\"Login-screen\"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.TextView[1]");
+    Assert.assertEquals(errorText.getText(), "Please enter a valid email address");
 
 
 
